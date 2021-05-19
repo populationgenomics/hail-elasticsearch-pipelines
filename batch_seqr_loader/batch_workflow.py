ce04e20a6f932e5d44beaa2e9dcdeef0591450e8
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-19 15:08:26 +1000

    Add --genomicsdb option

--- batch_seqr_loader/batch_workflow.py
+++ batch_seqr_loader/batch_workflow.py
@@ -19,9 +19,12 @@ import click
 import hailtop.batch as hb
 from analysis_runner import dataproc
 from hailtop.batch.job import Job
-import hail as hl
+from google.cloud import storage
 
 GATK_CONTAINER = 'broadinstitute/gatk:4.1.8.0'
+GATK_GCSFUSE_CONTAINER = (
+    'australia-southeast1-docker.pkg.dev/cpg-common/images/gatk_gcsfuse:v0'
+)
 
 BROAD_REF_BUCKET = 'gs://gcp-public-data--broad-references/hg38/v0'
 REF_FASTA = join(BROAD_REF_BUCKET, 'Homo_sapiens_assembly38.fasta')
@@ -48,6 +51,12 @@ logger.setLevel('INFO')
     'gvcf_buckets',
     multiple=True,
     required=True,
+)
+@click.option(
+    '--genomicsdb',
+    'genomicsdb_path',
+    required=True,
+    help='Bucket path to the genomics DB. If doesn\'t exist, a new one will be created',
 )
 @click.option(
     '--ped-file',
@@ -106,6 +115,7 @@ logger.setLevel('INFO')
 @click.option('--vep-block-size', 'vep_block_size')
 def main(
     gvcf_buckets: List[str],
+    genomicsdb_path: str,
     ped_fpath: str,
     dataset_name: str,
     dest_mt_path: str,
@@ -114,14 +124,14 @@ def main(
     batch_id_to_reuse_scratch: Optional[str],
     dry_run: bool,
     billing_project: Optional[str],
-    genome_version: str,
-    disable_validation: bool,
-    dataset_type: str,
-    sample_type: str,
-    remap_path: str = None,
-    subset_path: str = None,
-    vep_config_json_path: Optional[str] = None,
-    vep_block_size: Optional[int] = None,
+    genome_version: str,  # pylint: disable=unused-argument
+    disable_validation: bool,  # pylint: disable=unused-argument
+    dataset_type: str,  # pylint: disable=unused-argument
+    sample_type: str,  # pylint: disable=unused-argument
+    remap_path: str = None,  # pylint: disable=unused-argument
+    subset_path: str = None,  # pylint: disable=unused-argument
+    vep_config_json_path: Optional[str] = None,  # pylint: disable=unused-argument
+    vep_block_size: Optional[int] = None,  # pylint: disable=unused-argument
 ):
     """
     Entry point for a batch workflow
@@ -129,7 +139,7 @@ def main(
     if not billing_project:
         raise click.BadParameter('--billing-project has to be specified')
 
-    hail_bucket = os.environ.get('HAIL_BUCKET') or join(work_bucket, 'hail')
+    hail_bucket = join(work_bucket, 'hail')
     backend = hb.ServiceBackend(
         billing_project=billing_project,
         bucket=hail_bucket.replace('gs://', ''),
@@ -157,7 +167,7 @@ def main(
     batch_to_reuse_bucket = None
     if batch_id_to_reuse_scratch:
         batch_to_reuse_bucket = f'{hail_bucket}/batch/{batch_id_to_reuse_scratch}'
-    
+
     paths = None
     if batch_to_reuse_bucket:
         job_id = 1  # 0-based to 1-based index
@@ -165,7 +175,7 @@ def main(
             f'interval_{idx}': f'{batch_to_reuse_bucket}/{job_id}/intervals/{str(idx).zfill(4)}-scattered.interval_list'
             for idx in range(scatter_count)
         }
-    if paths and all(hl.hadoop_exists(path) for path in paths.values()):
+    if paths and all(file_exists(path) for path in paths.values()):
         intervals = b.read_input_group(**paths)
     else:
         split_intervals_job = add_split_intervals_job(
@@ -183,64 +193,54 @@ def main(
         sample_map_fpath, sep='\t', header=False, index=False
     )
     for idx in range(scatter_count):
-        path = None
-        if batch_id_to_reuse_scratch:
-            job_id = (
-                1 +  # 0-based to 1-based index
-                1 +  # split intervals job
-                idx * 2  # 2 jobs for each interval
-            )
-            path = f'{hail_bucket}/batch/{batch_id_to_reuse_scratch}/{job_id}/genomicsdb.tar'
-        if path and hl.hadoop_exists(path):
-            workspace_tar = b.read_input(path)
-        else:
-            import_gvcfs_job = add_import_gvcfs_job(
-                b=b,
-                sample_name_map=b.read_input(sample_map_fpath),
-                interval=intervals[f'interval_{idx}'],
-            )
-            workspace_tar = import_gvcfs_job["genomicsdb.tar"]
+        import_gvcfs_job = _add_import_gvcfs_job(
+            b=b,
+            genomicsdb_path=genomicsdb_path,
+            sample_name_map=b.read_input(sample_map_fpath),
+            interval=intervals[f'interval_{idx}'],
+        )
 
         paths = None
         if batch_id_to_reuse_scratch:
             job_id = (
-                1 +  # 0-based to 1-based index
-                1 +  # split_intervals job
-                1 +  # import_gvcfs job
-                idx * 2  # 2 jobs for each interval
+                1
+                + 1  # 0-based to 1-based index
+                + 1  # split_intervals job
+                + idx * 2  # import_gvcfs job  # 2 jobs for each interval
             )
             gvcf_path = f'{hail_bucket}/batch/{batch_id_to_reuse_scratch}/{job_id}/output_vcf.vcf.gz'
             paths = {
                 'vcf.gz': gvcf_path,
                 'vcf.gz.tbi': gvcf_path + '.tbi',
             }
-        if paths and all(hl.hadoop_exists(path) for path in paths.values()):
+        if paths and all(file_exists(path) for path in paths.values()):
             genotyped_vcfs.append(b.read_input_group(**paths))
         else:
-            genotype_vcf_job = add_gatk_genotype_gvcf_job(
+            genotype_vcf_job = _add_gatk_genotype_gvcf_job(
                 b,
-                workspace_tar=workspace_tar,
+                genomicsdb_path=genomicsdb_path,
                 interval=intervals[f'interval_{idx}'],
                 reference=reference,
                 dbsnp=dbsnp,
                 disk_size=100,
             )
+            genotype_vcf_job.depends_on(import_gvcfs_job)
             genotype_vcf_jobs.append(genotype_vcf_job)
             genotyped_vcfs.append(genotype_vcf_job.output_vcf)
 
     final_gathered_vcf_job = None
     gathered_vcf_path = join(work_bucket, f'{dataset_name}.vcf')
-    if batch_id_to_reuse_scratch and hl.hadoop_exists(gathered_vcf_path):
+    if batch_id_to_reuse_scratch and file_exists(gathered_vcf_path):
         pass
     else:
-        final_gathered_vcf_job = add_final_gather_vcf_step(
+        final_gathered_vcf_job = _add_final_gather_vcf_step(
             b,
             input_vcfs=genotyped_vcfs,
             disk_size=200,
             output_vcf_path=gathered_vcf_path,
         )
 
-    j = dataproc.hail_dataproc_job(
+    dataproc.hail_dataproc_job(
         b,
         f'batch_seqr_loader/seqr_load.py '
         f'--source-path {gathered_vcf_path} '
@@ -348,17 +348,31 @@ def add_split_intervals_job(
     return j
 
 
-def add_import_gvcfs_job(
+def _add_import_gvcfs_job(
     b: hb.Batch,
+    genomicsdb_path: str,
     sample_name_map: hb.ResourceFile,
     interval: hb.ResourceFile,
     disk_size: int = 30,
-):
+) -> Job:
+    """
+    Add GVCFs to a genomics database (or create a new instance if it doesn't exist).
+
+    Uses gcsfuse to mount the database to a local disk on an instance
+    """
     j = b.new_job('ImportGVCFs')
-    j.image(GATK_CONTAINER)
+    j.image(GATK_GCSFUSE_CONTAINER)
     mem_gb = 16
     j.memory(f'{mem_gb}G')
     j.storage(f'{disk_size}G')
+
+    if file_exists(genomicsdb_path):
+        # Update existing DB
+        genomicsdb_cmd = '--genomicsdb-update-workspace-path /genomicsdb_workspace'
+    else:
+        # Initiate new DB
+        genomicsdb_cmd = '--genomicsdb-workspace-path /genomicsdb_workspace'
+
     j.command(
         f"""set -e
 
@@ -378,29 +392,34 @@ def add_import_gvcfs_job(
     # within the task; please do not change it without consulting
     # the Hellbender (GATK engine) team!
 
+    gcsfuse {genomicsdb_path} /genomicsdb_workspace
+
     gatk --java-options -Xms{mem_gb - 1}g \
       GenomicsDBImport \
-      --genomicsdb-workspace-path /genomicsdb_workspace \
+      {genomicsdb_cmd} \
       --batch-size 50 \
       -L {interval} \
       --sample-name-map {sample_name_map} \
       --reader-threads 5 \
       --merge-input-intervals \
-      --consolidate
-
-    tar -cf {j["genomicsdb.tar"]} /genomicsdb_workspace"""
+      --consolidate"""
     )
     return j
 
 
-def add_gatk_genotype_gvcf_job(
+def _add_gatk_genotype_gvcf_job(
     b: hb.Batch,
-    workspace_tar: hb.ResourceFile,
+    genomicsdb_path: str,
     interval: hb.ResourceFile,
     reference: hb.ResourceGroup,
     dbsnp: hb.ResourceGroup,
     disk_size: int = 100,
 ) -> Job:
+    """
+    Run joint-calling on all samples in a genomics database
+
+    Uses gcsfuse to mount the database to a local disk on an instance
+    """
     j = b.new_job('GenotypeGVCFs')
     j.image(GATK_CONTAINER)
     j.memory(f'32G')
@@ -414,15 +433,14 @@ def add_gatk_genotype_gvcf_job(
 
     j.command(
         f"""set -e
-        
-    tar -xf {workspace_tar}
+
+    gcsfuse {genomicsdb_path} /genomicsdb_workspace
 
     gatk --java-options -Xms8g \\
       GenotypeGVCFs \\
       -R {reference.base} \\
-      -O {j.output_vcf} \\
+      -O {j.output_vcf['vcf.gz']} \\
       -D {dbsnp.base} \\
-      -G StandardAnnotation -G AS_StandardAnnotation \\
       --only-output-calls-starting-in-intervals \\
       -V gendb://genomicsdb_workspace \\
       -L {interval} \\
@@ -432,116 +450,14 @@ def add_gatk_genotype_gvcf_job(
     return j
 
 
-def add_import_gvcfs_step(
-    b,
-    sample_name_map,
-    interval,
-    ref_fasta,
-    ref_fasta_index,
-    ref_dict,
-    workspace_dir_name,
-    disk_size,
-    batch_size,
-):
-    j = b.new_job("ImportGVCFs")
-    j.image(container)
-    j.memory(f"24.214398G")
-    j.storage(f'{(("local-disk " + disk_size) + " HDD")}G')
-
-    j.command(
-        f"""set -euo pipefail
-
-    rm -rf workspace_dir_name
-
-    # We've seen some GenomicsDB performance regressions related to intervals, so we're going to pretend we only have a single interval
-    # using the --merge-input-intervals arg
-    # There's no data in between since we didn't run HaplotypeCaller over those loci so we're not wasting any compute
-
-    # The memory setting here is very important and must be several GiB lower
-    # than the total memory allocated to the VM because this tool uses
-    # a significant amount of non-heap memory for native libraries.
-    # Also, testing has shown that the multithreaded reader initialization
-    # does not scale well beyond 5 threads, so don't increase beyond that.
-    gatk --java-options -Xms8g \
-      GenomicsDBImport \
-      --genomicsdb-workspace-path workspace_dir_name \
-      --batch-size batch_size \
-      -L interval \
-      --sample-name-map sample_name_map \
-      --reader-threads 5 \
-      --merge-input-intervals \
-      --consolidate
-
-    tar -cf workspace_dir_name.tar workspace_dir_name"""
-    )
-
-    j.command(
-        'ln "{value}" {dest}'.format(
-            value=f"workspace_dir_name.tar", dest=j.output_genomicsdb
-        )
-    )
-    return j
-
-
-def add_genotype_gvcfs_step(
-    b,
-    workspace_tar,
-    interval,
-    output_vcf_filename,
-    ref_fasta,
-    ref_fasta_index,
-    ref_dict,
-    dbsnp_vcf,
-    disk_size,
-    allow_old_rms_mapping_quality_annotation_data=False,
-    gatk_docker="us.gcr.io/broad-gatk/gatk:4.1.8.0",
-    container="us.gcr.io/broad-gatk/gatk:4.1.8.0",
-):
-    j = b.new_job("GenotypeGVCFs")
-    j.image(container)
-    j.memory(f"24.214398G")
-    j.storage(f'{(("local-disk " + disk_size) + " HDD")}G')
-
-    j.command(
-        f"""set -euo pipefail
-
-    tar -xf workspace_tar
-    WORKSPACE=$(basename workspace_tar .tar)
-
-    gatk --java-options -Xms8g \
-      GenotypeGVCFs \
-      -R ref_fasta \
-      -O output_vcf_filename \
-      -D dbsnp_vcf \
-      -G StandardAnnotation -G AS_StandardAnnotation \
-      --only-output-calls-starting-in-intervals \
-      -V gendb://$WORKSPACE \
-      -L interval \
-      allow_old_rms_mapping_quality_annotation_data \
-      --merge-input-intervals"""
-    )
-
-    j.command(
-        'ln "{value}" {dest}'.format(value=f"output_vcf_filename", dest=j.output_vcf)
-    )
-    j.command(
-        'ln "{value}" {dest}'.format(
-            value=f"output_vcf_filename.tbi", dest=j.output_vcf_index
-        )
-    )
-
-    return j
-    b.run(dry_run=True)
-
-
-def add_final_gather_vcf_step(
+def _add_final_gather_vcf_step(
     b: hb.Batch,
     input_vcfs: List[hb.ResourceGroup],
     disk_size: int,
     output_vcf_path: str = None,
 ) -> Job:
     """
-    Combines recalibrated VCFs into a single VCF.
+    Combines per-interval scattered VCFs into a single VCF.
     Saves the output VCF to a bucket `output_vcf_path`
     """
     j = b.new_job('FinalGatherVcf')
@@ -572,6 +488,29 @@ def add_final_gather_vcf_step(
     if output_vcf_path:
         b.write_output(j.output_vcf, output_vcf_path.replace('.vcf.gz', ''))
     return j
+
+
+def file_exists(path: str) -> bool:
+    """
+    Check if the object exists, where the object can be:
+        * local file
+        * local directory
+        * Google Storage object
+        * Google Storage URL representing a *.mt or *.ht Hail data,
+          in which case it will check for the existence of a
+          *.mt/_SUCCESS or *.ht/_SUCCESS file.
+    :param path: path to the file/directory/object/mt/ht
+    :return: True if the object exists
+    """
+    if path.startswith('gs://'):
+        bucket = path.replace('gs://', '').split('/')[0]
+        path = path.replace('gs://', '').split('/', maxsplit=1)[1]
+        path = path.rstrip('/')  # '.mt/' -> '.mt'
+        if any(path.endswith(f'.{suf}') for suf in ['mt', 'ht']):
+            path = os.path.join(path, '_SUCCESS')
+        gs = storage.Client()
+        return gs.get_bucket(bucket).get_blob(path)
+    return os.path.exists(path)
 
 
 if __name__ == '__main__':
