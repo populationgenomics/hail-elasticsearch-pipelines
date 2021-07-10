c089f6aeeb6926ea265f14d80039bd6bf7c2953b
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-07-10 17:17:08 +1000

    Move sample-specific information to columns

--- parquet/annotated_mt_to_parquet.py
+++ parquet/annotated_mt_to_parquet.py
@@ -3,13 +3,52 @@ from analysis_runner import bucket_path, output_path
 
 hl.init(default_reference='GRCh38')
 
-mt = hl.read_matrix_table(bucket_path('seqr-loader-tmp/zornitza-stark-kccg-gvcf/v1-0/jointly-called/680e476abb7a1e4f9ba35a97d38d273cb4e68ba1390a51bf3ce1b893/annotated.mt', 'tmp'))
+mt = hl.read_matrix_table(
+    bucket_path(
+        'seqr-loader-tmp/acute-care/v1-0/jointly-called/680e476abb7a1e4f9ba35a97d38d273cb4e68ba1390a51bf3ce1b893/annotated.mt',
+        'tmp',
+    )
+)
+
+# Discard VEP, which was only used to derive other annotations.
+mt = mt.drop(mt.vep)
+mt.write(output_path('no_vep.mt', 'tmp'))
 
 # See https://github.com/populationgenomics/hail-elasticsearch-pipelines/blob/91f6adf07d4708391340dd2f568fca7d166c5735/luigi_pipeline/lib/model/seqr_mt_schema.py#L304.
-ht = mt.rows()
-ht = ht.drop(ht.vep)
-ht = ht.flatten()
+ht = mt.rows().flatten()
 ht = ht.drop(ht.locus, ht.alleles)
+ht.write(output_path('no_vep.ht', 'tmp'))
 
 df = ht.to_spark()
-df.write.option('compression', 'zstd').parquet(output_path('table.parquet', 'tmp'))
+df.write.option('compression', 'none').parquet(
+    output_path('original_uncompressed.parquet', 'tmp')
+)
+df.write.option('compression', 'zstd').parquet(output_path('original_zstd.parquet', 'tmp'))
+
+# Drop all sample-specific fields.
+mt = mt.drop(
+    mt.genotypes, mt.samples_no_call, mt.samples_num_alt, mt.samples_gq, mt.samples_ab
+)
+ht = mt.rows().flatten()
+ht = ht.drop(ht.locus, ht.alleles)
+df = ht.to_spark()
+df.write.option('compression', 'zstd').parquet(
+    output_path('sites_only_zstd.parquet', 'tmp')
+)
+
+# Annotate entries with genotype information instead.
+mt = mt.select_entries(
+    num_alt=mt.GT.n_alt_alleles(),
+    gq=mt.GQ,
+    ab=hl.bind(
+        lambda total: hl.or_missing(total > 0 & hl.len(mt.AD) > 1, mt.AD[1] / total),
+        hl.sum(mt.AD),
+    ),
+    dp=mt.DP,
+)
+ht = mt.make_table()
+ht = ht.drop(ht.locus, ht.alleles)
+df = ht.to_spark()
+df.write.option('compression', 'zstd').parquet(
+    output_path('genotype_columns.parquet', 'tmp')
+)
