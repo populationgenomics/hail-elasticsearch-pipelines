2b1b76e9d845843104272f19869bd05d473f1033
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-07-21 12:36:31 +1000

    Add test job that just tries bwa

---
+++ batch_seqr_loader/try_batch_job.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import hailtop.batch as hb
+from os.path import join
+
+# import hailtop.batch as hb
+# backend = hb.ServiceBackend()
+# batch = hb.Batch(backend=backend, name='test-batch')
+
+# j = batch.new_job(name='test-job')
+# j.image('ubuntu:18.04')
+# j.command(f'''
+# echo "test" > {j.ofile}
+# ''')
+# batch.write_output(j.ofile, 'gs://playground-us/batch-test/test.done')
+# batch.run(open=True, wait=False)
+# backend.close()
+
+BAZAM_CONTAINER = f'australia-southeast1-docker.pkg.dev/cpg-common/images/bazam:v2'
+REF_BUCKET = 'gs://cpg-reference/hg38/v1'
+REF_FASTA = join(REF_BUCKET, 'Homo_sapiens_assembly38.fasta')
+
+
+def _make_realign_jobs(
+    b: hb.Batch,
+    reference: hb.ResourceGroup,
+):
+    fq1 = b.read_input('gs://cpg-seqr-test/batches/test/tmp_fq')
+    j = b.new_job('Test BWA')
+    j.image(BAZAM_CONTAINER)
+    total_cpu = 16
+    bazam_cpu = 3
+    bwa_cpu = 10
+    bamsormadup_cpu = 3
+    bwa_cpu = 1
+    j.memory('highmem')
+    j.cpu(total_cpu)
+    j.storage('300G')
+    j.declare_resource_group(
+        output_cram={
+            'base': '{root}.cram',
+            'crai': '{root}.crai',
+        }
+    )
+    sn = 'TEST'
+    rg_line = f'@RG\\tID:{sn}\\tSM:~{sn}'
+    # bwa index {reference.base}
+    use_bazam = True
+
+    j.command(
+        f"""
+set -o pipefail
+set -ex
+
+(while true; do df -h; pwd; ls | grep -v proc | xargs du -sh; free -m; sleep 300; done) &
+
+bwa mem -K 100000000 {'-p' if use_bazam else ''} -v3 -t{bwa_cpu} -Y \\
+-R '{rg_line}' {reference.base} \\
+{fq1} - > {j.aligned_sam}
+
+bamsormadup inputformat=sam threads={bamsormadup_cpu} SO=coordinate \\
+M={j.duplicate_metrics} outputformat=sam < {j.aligned_sam} | \\
+samtools view -T {reference.base} -O cram -o {j.output_cram.base}
+
+samtools index -@{total_cpu} {j.output_cram.base} {j.output_cram.crai}
+
+df -h; pwd; ls | grep -v proc | xargs du -sh
+    """
+    )
+
+
+backend = hb.ServiceBackend('vladislavsavelyev-trial', 'playground-au')
+b = hb.Batch(backend=backend, name='test')
+reference = b.read_input_group(
+    base=REF_FASTA,
+    fai=REF_FASTA + '.fai',
+    dict=REF_FASTA.replace('.fasta', '').replace('.fna', '').replace('.fa', '')
+    + '.dict',
+)
+_make_realign_jobs(b, reference)
+b.run(open=True)
