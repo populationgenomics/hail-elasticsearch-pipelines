bde13db079607a8427a62dd159b5b6438300e875
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-08-12 21:55:49 +1000

    Pin SM to 2.0.3

--- batch_seqr_loader/scripts/list_sm_samples.py
+++ batch_seqr_loader/scripts/list_sm_samples.py
@@ -14,10 +14,10 @@ PROJ = os.environ.get('SM_DEV_DB_PROJECT', 'vladdev')
 sapi = SampleApi()
 aapi = AnalysisApi()
 
-samples = sapi.get_all_samples(PROJ)
+samples = sapi.get_samples()
 projects = set()
 for s in samples:
-    projects.add(s.meta.get('project'))
+    projects.add(s['meta'].get('project'))
 print(f'Projects: {projects}')
 print('Samples:')
 for s in samples:
