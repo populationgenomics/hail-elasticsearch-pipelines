8dd4b8470bd1ad23e1e70c651cb156548d3ae788
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-08-23 10:50:34 +1000

    Index bwa-mem2 change bucket

--- batch_seqr_loader/test/batch_index_bwa.py
+++ batch_seqr_loader/test/batch_index_bwa.py
@@ -8,7 +8,7 @@ import os
 
 CONTAINER = f'australia-southeast1-docker.pkg.dev/cpg-common/images/alignment:v4'
 REF_BUCKET = 'gs://cpg-reference/hg38/v1'
-TARGET_BUCKET = 'gs://cpg-reference/hg38/v1'
+TARGET_BUCKET = 'gs://playground-au/cpg-reference/hg38/v1'
 REF_FASTA = join(REF_BUCKET, 'Homo_sapiens_assembly38.fasta')
 TARGET_FASTA = join(TARGET_BUCKET, 'Homo_sapiens_assembly38.fasta')
 
