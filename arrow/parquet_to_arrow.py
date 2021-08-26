aae6fbcf61883f08148420b5684f12c954ea841c
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-08-26 18:26:42 +1000

    fix

---
+++ arrow/parquet_to_arrow.py
@@ -0,0 +1,34 @@
+"""Converts from Parquet to Arrow IPC."""
+
+import click
+import math
+import google.cloud.storage as gcs
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+
+@click.command()
+@click.option('--input', help='Input path for Parquet partitions', required=True)
+@click.option(
+    '--scatter_index', help='Scatter index over partitions', type=int, required=True
+)
+@click.option(
+    '--scatter_count', help='Scatter count over partitions', type=int, required=True
+)
+def parquet_to_arrow(input, scatter_index, scatter_count):
+    gcs_client = gcs.Client()
+
+    input_parts = input.split('/')
+    input_bucket = input_parts[2]
+    input_blob_prefix = '/'.join(input_parts[3:])
+
+    all_files = gcs_client.list_blobs(input_bucket, prefix=input_blob_prefix)
+    num_files = len(all_files)
+    per_shard = math.ceil(num_files / scatter_count)
+    shard_files = all_files[shard_index * per_shard : (shard_index + 1) * per_shard]
+    for file in shard_files:
+        print(file)
+
+
+if __name__ == '__main__':
+    parquet_to_arrow()  # pylint: disable=no-value-for-parameter
